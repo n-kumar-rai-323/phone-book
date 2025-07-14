8ea7b1f391d1a0bc3328c6dd008533d238a4a012
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2025-07-14 22:18:27 +0545

    Project setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("http")
+const app = require("./src/config/express.config")
+const PORT = Number(process.env.PORT || 8000)
+const server = http.createServer(app)
+
+server.listen(PORT, 'localhost', (err) => {
+    if (!err) {
+        console.log(`Application Running ${PORT}`)
+    }
+})
